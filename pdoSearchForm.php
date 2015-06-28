c10a4da5d14cacc3f546373e41640e586907c195
geekgeek <solq87@gmail.com>
2015-06-28 17:07:31 +0200

    Update pdoSearchForm.php

--- pdoSearchForm.php
+++ pdoSearchForm.php
@@ -2,9 +2,6 @@
 
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
 -->
 <html>
     <head>
